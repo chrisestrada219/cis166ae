d7adea54421a54e387a200677ab42f24020e8435
chrisestrada219 <60501125+chrisestrada219@users.noreply.github.com>
2020-02-09 22:14:59 -0700

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Variables</title>
+</head>
+<body>
+<?php // Script 2.3 - variables.php 
+
+$first_name = "Christian";
+$last_name = "Estrada";
+$street = "1234 W College Street";
+$city = "Phoenix";
+$state = "AZ";
+$zip = 85007; 
+$number = "281-330-8004";
+
+print "<p>$first_name $last_name's contact info: <br>$street 
+		<br>$city $state $zip <br>$number</p>"
+?>
+</body>
+</html>
