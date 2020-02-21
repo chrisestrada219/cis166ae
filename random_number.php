8ad3892c913df8c1f063357cfa3ac1030f8a54c0
chrisestrada219 <60501125+chrisestrada219@users.noreply.github.com>
2020-02-20 20:55:35 -0700

    Add files via upload

---
+++ random_number.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Random Number</title>
+</head>
+<body>
+<?php
+	echo(rand() . "<br>");
+	echo(rand() . "<br>");
+	echo(rand(10,100));
+?>
+</body>
+</html>
