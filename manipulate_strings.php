807785ea19539a1f45c67dda836dfba54cceb48a
chrisestrada219 <60501125+chrisestrada219@users.noreply.github.com>
2020-03-01 23:42:12 -0700

    Add files via upload

---
+++ manipulate_strings.php
@@ -0,0 +1,13 @@
+<?php
+
+echo str_word_count
+("I love The Raven by author, Edgar Allan Poe"); 
+// There are 9 words in the this string
+
+echo strcmp ("Edgar Allan Poe","Edgar Allan Poe"); 
+// These two strings are equal
+
+echo strlen ("Author Edgar Allan Poe");
+// This string has 22 characters 
+
+?>
