fa19fbef476b76209e9565a15d102d26d96ab6ad
chrisestrada219 <60501125+chrisestrada219@users.noreply.github.com>
2020-03-08 14:21:25 -0700

    Add files via upload

---
+++ handle_reg.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang=“en”>
+<head>
+  <meta charset=“utf-8”>
+  <title>Registration</title>
+</head>
+
+<body>
+
+<h1>Registration Results</h1>
+
+<?php // Script 6.2 - handle_reg.php
+error_reporting(E_ALL);
+
+$okay = true;
+
+if ($okay) {
+	print '<p>You have been successfully
+	registered (but not really).</p>';
+	}
+?>
+</body>
+</html>
