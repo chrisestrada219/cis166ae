0764792a89ce8844e7f78cee797f2dfa7882278d
chrisestrada219 <60501125+chrisestrada219@users.noreply.github.com>
2020-03-08 20:09:44 -0700

    Add files via upload

---
+++ handle_reg3.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang=“en”>
+<head>
+  <meta charset=“utf-8”>
+  <title>Registration</title>
+  
+  <style type="text/css" media="screen">
+		.error {color: red; }
+  </style>   
+</head>
+
+<body>
+
+<h1>Registration Results</h1>
+
+<?php // Script 6.2 - handle_reg.php
+error_reporting(E_ALL);
+
+$okay = true;
+
+if (empty($_POST['email'])) {
+	print '<p class="error"> Please
+	enter your email address.</p>';
+	$okay = false;
+	}
+	
+if (empty($_POST['password'])) {
+	print '<p class="error">Please enter
+	your password.</p>'; 
+	$okay = false;
+	}
+
+if (is_numeric($_POST['year'])) {
+	$age = 2016 - $_POST['year'];
+	} else {
+	print '<p class="error">Please enter
+	the year you were born as four 
+	digits.</p>';
+	$okay = false;
+	}
+
+if ($okay) {
+	print '<p>You have been successfully
+	registered (but not really).</p>';
+	print "<p>You will turn $age this 
+	year.</p>";
+	}
+	
+?>
+</body>
+</html> 
