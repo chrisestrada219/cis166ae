81ce6b6fa85178d3673ab878334f9692dcec760e
chrisestrada219 <60501125+chrisestrada219@users.noreply.github.com>
2020-03-15 00:39:46 -0700

    Add files via upload

---
+++ soups1.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>No Soup For You!</title>
+</head>
+<body>
+<h1>Mmm...soups</h1>
+
+<?php // Script 7.1 - soups1.php
+
+$soups = [
+'Monday' => 'Clam Chowder',
+'Tuesday' => 'White Chicken Chili',
+'Wednesday' => 'Vegetarian',
+'Thursday' => 'Chicken Soup',
+'Friday' => 'Tomato Soup',
+'Saturday' => 'Menudo',
+'Sunday' => 'French Onion Soup'
+];
+
+print "<p>$soups</p>";
+
+print_r($soups);
+
+?>
+</body>
+</html>	
