65e9a15a333762e3733929cab9e0c42ed5b1b13e
chrisestrada219 <60501125+chrisestrada219@users.noreply.github.com>
2020-03-22 21:59:57 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php // Script 8.4 - index.php
+/* This is the home page for the 
+	site.
+It uses templates to create the
+	layout. */
+
+error_reporting(E_ALL);
+
+include('templates/header.html');
+
+?>
+
+<h2>Welcome to a J.D. Salinger
+	Fan Club</h2>
+<p>Lorem ipsum dolor sit
+	amet...</p>
+
+<?php
+
+include('templates/footer.html');
+
+?>
