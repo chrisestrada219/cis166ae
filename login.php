850320cf40cc3886ae4af53afb7ed87c9e5fa90d
chrisestrada219 <60501125+chrisestrada219@users.noreply.github.com>
2020-04-30 17:31:01 -0700

    Add files via upload

--- login.php
+++ login.php
@@ -1,74 +1,64 @@
-<?php // Script 8.8 - login.php
+<?php // Script 13.5 - login.php
+
+$loggedin = false;
+$error = false;
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	
+	if (!empty($_POST['email']) && 
+	!empty($_POST['password'])) {
+	
+		if ( (strtolower($_POST['email']) == 
+		'me@example.com') && 
+		($_POST ['password'] ==
+		'testpass') ) {
+			
+			setcookie('Daria', 'Lane', 
+			time()+3600);
+					
+			$loggedin = true;
+					
+		} else {
+				
+			$error = 'The submitted email
+			address and password do not 
+			match those on file!';
+		
+		}
+
+	} else {
+
+		$error = 'Please make sure you 
+		enter both an email and a 
+		password!';
+
+	}
+}
 
 define('TITLE', 'Login');
 include('templates/header.html');
 
-print '<h2>Login Form</h2>
-	<p>Users who are logged in
-		can take advantage of certain 
-		features like this, that, and 
-		the other things.</p>';
+if ($error) {
+	print '<p class="error">' . $error . '</p>';
+}
 
-if ($_SERVER['REQUEST_METHOD'] ==
-	'POST') {
-		
-		if ( (!empty($_POST['email'])) && 
-		(!empty($_POST['password'])) ) {
-		
-			if ( (strtolower
-				($_POST['email']) ==
-				'me@example.com') && 
-				($_POST['password'] == 
-				'testpass') ) { // Correct!
-				
-				session_start();
-				$_SESSION['email'] = 
-					$_POST['email'];
-				$_SESSION['loggedin'] = time();
-				
-				ob_end_clean();
-				header('Location: welcome2.php');
-				exit();
-					
-			} else { // Incorrect!
-			
-				print '<p class="text--
-					error">The submitted email
-					address and password do not 
-					match those on file!<br>Go
-					back and try again.</p>';
-					
-				}
-				
-		} else { // Forgot a field.
-		
-			print '<p class="text--
-			error">Please make sure you
-			enter both an email address
-			and a password!<br>Go back
-			and try again.</p>';
-			
-			}
-			
-		} else { // Display the form.
-		
-			print '<form action=
-				"login.php" method="post"
-				class="form--inline">
-			<p><label for="email">
-				Email Address:</label>
-				<input tye="email"
-				name="email" size="20"></p>
-			<p><label for="password">
-				Password:</label><input type=
-				"password" name="password"
-				size="20"></p>
-			<p><input type="submit" name=
-				"submit" value="Log In!"
-				class="button--pill"></p>
-			</form>';
-			
-		}
+if ($loggedin) {
+
+	print '<p>You are now logged in!</p>';
+	
+} else {
+
+	print '<h2>Login Form</h2>
+	<form action="login.php" method="post">
+	<p><label>Email Address <input type="email" 
+		name="email"></label></p>
+	<p><label>Password <input type="password" 
+		name="password"></label></p>
+	<p><input type="submit" name="submit" 
+		value="Log In!"></p>
+	</form>';
+	
+}
 
 include('templates/footer.html');
 
